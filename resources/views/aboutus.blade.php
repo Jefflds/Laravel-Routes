06a242dce5025c91043a3e1aad4d41044b5639ab
jeffersonlds3009 <jeffersonlds3009@gmail.com>
2024-02-23 00:43:03 +0000

    Mudando estilização

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,16 +1,30 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>About Us</title>
     @vite('resources/css/app.css')
- </head>
-<body class="bg-gray-100 py-10">
-    <div class="max-w-4xl mx-auto px-4">
+</head>
+
+<body class="bg-gray-100">
+    <nav class="bg-gray-800 text-white py-4">
+        <div class="max-w-7xl mx-auto px-4 flex justify-between items-center">
+            <div>
+                <a href="/" class="text-blue-200 hover:text-white mx-2">Home</a>
+                <a href="/contactUs" class="text-blue-200 hover:text-white mx-2">Contact Us</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="max-w-4xl mx-auto px-4 mt-8">
         <h1 class="text-3xl font-bold text-gray-800 mb-8">About Us Page</h1>
-        <a href="/" class="text-blue-500 hover:underline mr-4">Home</a>
-        <a href="/contactUs" class="text-blue-500 hover:underline">Contact Us</a>
+        <div class="text-gray-700">
+            <p class="mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt quisquam illum veniam nihil officiis, tenetur illo earum rerum est iste reprehenderit incidunt perferendis quam quidem aspernatur optio ab labore dicta!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, modi voluptatum. Deleniti aspernatur minima voluptatum reiciendis animi corporis labore, eaque libero eius, laboriosam repellendus laborum totam fugit veniam quas explicabo!</p>
+        </div>
     </div>
 </body>
+
 </html>
